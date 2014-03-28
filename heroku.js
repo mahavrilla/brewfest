b38c0a7d4099f7606a0c69101b9dd416b6c9bc3f
Mike Havrilla <havs@Mikes-MacBook-Pro.local>
2014-03-28 15:04:07 -0700

    intiial commit

---
+++ heroku.js
@@ -0,0 +1,35 @@
+var deployd = require('deployd');
+ 
+// configure database etc. 
+var server = deployd({
+  port: process.env.PORT || 5000,
+  env: 'production',
+  db: {
+    host: 'oceanic.mongohq.com',
+    port: 10071,
+    name: 'app23490174',
+    credentials: {
+      username: 'heroku',
+      password: '22767b9fd645bf0cb5adb4c8c4add817'
+    }
+  }
+});
+ 
+// heroku requires these settings for sockets to work
+server.sockets.manager.settings.transports = ["xhr-polling"];
+ 
+// start the server
+server.listen();
+ 
+// debug
+server.on('listening', function() {
+  console.log("Server is listening on port: " + process.env.PORT);
+});
+ 
+// Deployd requires this
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
+});
