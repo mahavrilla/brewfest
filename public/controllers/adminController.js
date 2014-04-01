6cd90c4fed204e68a6b79a81d1666bce6582d020
Mike Havrilla <havs@Mikes-MacBook-Pro.local>
2014-04-01 12:38:32 -0700

    added admin stuff

--- public/controllers/adminController.js
+++ public/controllers/adminController.js
@@ -13,6 +13,8 @@ votingApp.controller('adminController', function($scope, $http, $location, admin
 	$scope.predicate = '-rating';
 	$scope.showBeer = true;
 	$scope.showChili = false;
+	$scope.showEnter = false;
+	$scope.newItems = [];
 
 	$scope.loadData = function()  {
 		$http.get('/votes', { 
@@ -78,20 +80,91 @@ votingApp.controller('adminController', function($scope, $http, $location, admin
 	    $scope.showBeerVotes = function() {
 	    	$scope.showChili = false;
 	    	$scope.showBeer = true;
+	    	$scope.showEnter = false;
 	    }
 
 	     $scope.showChiliVotes = function() {
 	    	$scope.showChili = true;
 	    	$scope.showBeer = false;
+	    	$scope.showEnter = false;
 	    }
 
 	    $scope.showEnterItems = function() {
 	    	$scope.showChili = false;
 	    	$scope.showBeer = false;
+	    	$scope.showEnter = true;
 	    }
 
 	    $scope.refreshVotes = function() {
 	    	$scope.loadData();
+	    }
+
+	    $scope.addNewItem = function() {
+	    	$scope.newItems.push({ name: '', isBeer: false, isBrewersChoice: false, isNew : true});
+	    }
+
+	    $scope.removeNewItem = function(index) {
+	    	$scope.removeFromDatabase( $scope.newItems[index].id, $scope.newItems[index].isBeer );
+	    	$scope.newItems.splice(index, 1);
+	    }
+
+	    $scope.removeFromDatabase = function(id, isBeer) {
+	    	alert(id + isBeer);
+	    	if(isBeer) {
+	    		$http.delete('/beers?id=' + id, { 
+		 		}).success(function(result) {
+	 			});
+	    	}
+	    	
+	    }
+
+	    $scope.getItems = function() {
+	    	$http.get('/beers', { 
+	 		}).success(function(result) {
+	 			if(result.length == 0) {
+	 				$scope.newItems.push({ id: '', name: '', isBeer: false, isBrewersChoice: false, disabled: false})
+	 			} 
+	 			angular.forEach(result, function(value, key) {
+	 				$scope.newItems.push( { id: value.id, name: value.name, isBeer: true, isBrewersChoice: value.isBrewersChoice, disabled: true});
+	 			});
+	 		});
+	 		$http.get('/chili', { 
+	 		}).success(function(result) {
+	 			if(result.length == 0) {
+	 				$scope.newItems.push({ id: '', name: '', isBeer: false, isBrewersChoice: false})
+	 			} 
+	 			angular.forEach(result, function(value, key) {
+	 				$scope.newItems.push( value );
+	 			});
+	 		});
+	    }
+
+	    $scope.getItems();
+
+
+	    $scope.insertNewItems = function() {
+	    	$scope.newBeerItems = [];
+	    	$scope.newChilItems = [];
+
+	    	angular.forEach($scope.newItems, function(value, key) {
+	    		if(value.isBeer && value.name && value.isNew) {
+	    			$scope.insertBeers(value);
+	    			value.disabled = true;
+	    		} else if(value.name) {
+	    			$scope.newChilItems.push(value);
+	    		} 
+	    	});
+	    }
+
+	    $scope.insertBeers = function(beer) {
+	    	$http.post('/beers', {
+	    		name: beer.name,
+	    		isBrewersChoice: beer.isBrewersChoice
+		    }).success( function(beer) {
+		    }).error(function(err) {
+		      // Alert if there's an error
+		      return alert(err.message || "an error occurred");
+		    });
 	    }
 
 });
